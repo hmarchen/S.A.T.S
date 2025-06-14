4858b39e33b5c36cae22d590ecc8a7a94dd8e456
hmarchen <156877222+hmarchen@users.noreply.github.com>
2025-06-13 21:47:36 -0400

    css fix

--- app/css/styles.js
+++ app/css/styles.js
@@ -243,7 +243,7 @@ input: {
 
   textLink: {
     textAlign: 'center',
-    color: 'white',
+    color: 'cyan',
     fontSize: 18,
     paddingBottom: 20,
   },
@@ -498,6 +498,47 @@ errorText: {
   textAlign: 'center',
 },
 
+// *** MODAL STUFF ***
+
+modalOverlay: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "rgba(0,0,0,0.3)",
+},
+
+modalContent: {
+  backgroundColor: "white",
+  padding: 24,
+  borderRadius: 12,
+  alignItems: "center",
+  maxWidth: 900,
+},
+
+title: {
+  fontWeight: "bold",
+  fontSize: 18,
+  marginBottom: 10,
+  textAlign: "center"
+},
+
+subtitle: {
+  marginBottom: 15,
+  textAlign: "center"
+},
+
+closeButton: {
+  marginTop: 20,
+  backgroundColor: "#2196F3",
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  borderRadius: 8,
+},
+
+closeText: {
+  color: "white",
+  fontWeight: "bold",
+},
 });
 
 export default styles;
